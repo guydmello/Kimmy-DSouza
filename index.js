a54d02174e976bc5694a5969c65e2fcf0bf4c5d0
unknown <guyrdmello@gmail.com>
2022-12-04 21:54:17 -0500

    contact page done

--- index.js
+++
@@ -1,7 +0,0 @@
-document.getElementById("form").addEventListener("submit", function(e){
-    e.preventDefault();
-    var email = document.getElementById("email").value;
-    window.open('mailto:'+email);
-    
- 
- })
